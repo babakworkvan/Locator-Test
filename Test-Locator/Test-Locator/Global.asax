ffa959c3a2b773c518b06ddb49598c984d292780
Mohebbi <Babak.Mohebbi@bccourts.ca>
2021-05-25 15:02:36 -0700

    Raw MVC Project

---
+++ Test-Locator/Test-Locator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test_Locator.MvcApplication" Language="C#" %>
---
+++ Test-Locator/Test-Locator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Locator.Startup))]
+namespace Test_Locator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
